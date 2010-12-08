14e196fb1e1d383704c9a726172a4bd12138be95
matus.majchrak <matus.majchrak@7d530113-7ede-09e9-938b-5398bb9c765e>
2010-12-08 18:14:47 +0000

    Added register, login, infrastructure

--- finavi/ejb/src/main/java/com/finavi/ejb/UserServiceRemote.java
+++ finavi/ejb/src/main/java/com/finavi/ejb/UserServiceRemote.java
@@ -8,9 +8,17 @@ public interface UserServiceRemote {
 	public List<User> getAll();
 	
 	public User getById(long id);
-	
+	/**
+	 * Pridat noveho pouzivatela.
+	 * @param user
+	 * @return
+	 */
 	public User add(User user);
-	
+	/**
+	 * 
+	 * @param user
+	 * @return
+	 */
 	public User update(User user);
 	
 	public User delete(User user);
@@ -18,5 +26,9 @@ public interface UserServiceRemote {
 	public User getByName(String name);
 	
 	public User getByEmail(String email);
+	
+	public boolean register(User user);
+	
+	public User login(String email, String password);
 	
 }
