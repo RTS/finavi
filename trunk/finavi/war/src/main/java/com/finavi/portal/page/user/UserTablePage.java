2ef74e52b18abf4f54eda5dc24cfb32f53684832
michalsomora@gmail.com <michalsomora@gmail.com@7d530113-7ede-09e9-938b-5398bb9c765e>
2010-12-13 09:11:55 +0000

    export userov do excelu

--- trunk/finavi/war/src/main/java/com/finavi/portal/page/user/UserTablePage.java
+++ trunk/finavi/war/src/main/java/com/finavi/portal/page/user/UserTablePage.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.wicket.RequestCycle;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.TextFilteredPropertyColumn;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.WebResponse;
+import org.apache.wicket.request.target.component.ComponentRequestTarget;
 
 import com.finavi.model.User;
 import com.finavi.portal.page.base.AutheticatedPage;
@@ -48,9 +52,29 @@ public class UserTablePage extends AutheticatedPage {
 		collumns.add(new PropertyColumn<User>(new Model<String>("Email"),
 				"email"));
 
-		DefaultDataTable<User> userTable = new DefaultDataTable<User>(
+		final DefaultDataTable<User> userTable = new DefaultDataTable<User>(
 				"userListTable", collumns, provider, 30);
 
 		add(userTable);
+		
+		Link exportExcel = new Link("exportExcel") {
+            @Override
+            public void onClick() {
+                getRequestCycle().setRequestTarget(new ComponentRequestTarget(userTable) {
+                	@Override
+                	   public void respond( RequestCycle requestCycle) {
+                	        final int oldRowsPerPage = userTable.getRowsPerPage();
+                	        userTable.setRowsPerPage (Integer.MAX_VALUE);
+                	        super.respond(requestCycle);
+                	        userTable.setRowsPerPage(oldRowsPerPage);
+                	  }
+                });
+                WebResponse wr = (WebResponse) getResponse();
+                wr.setContentType("application/vnd.ms-excel; charset=UTF-8");
+                wr.setCharacterEncoding("UTF-8");
+                wr.setHeader("content-disposition", "attachment;filename=excel.xls");
+            }
+		};
+		add(exportExcel);
 	}
 }
